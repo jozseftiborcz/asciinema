5965baeb42c1996910665acd7a7e62957747527e
József Tiborcz <jozsef.tiborcz@gmail.com>
2022-08-05 15:33:29 +0200

    casteditor skeleton added

---
+++ asciinema/casteditor/__init__.py
@@ -0,0 +1,4 @@
+
+# CastEditor uses in cast markups to edit the asciinema cast. Currently it support only v2 version.
+class CastEditor:
+    pass
---
+++ tests/casteditor/casteditor_test.py
@@ -0,0 +1,9 @@
+from asciinema.asciicast import v2
+from asciinema.casteditor import CastEditor
+from ..test_helper import Test
+
+
+class TestCastEditorCommands(Test):
+    @staticmethod
+    def test_shrink() -> None:
+        assert True
